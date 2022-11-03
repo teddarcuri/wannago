eea2dfb0798bf119576c2e1a8de39a7641e4df09
Tedd Arcuri <tedd.arcuri@gmail.com>
2022-11-02 21:25:01 -0600

    Refactored to use default layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,36 @@
+<div id="app-wrapper">
+	<header id="app-nav">
+		<div id="logo">wannago</div>
+	</header>
+	<div id="app-body">
+		<slot />
+	</div>
+</div>
+
+<style>
+	#app-wrapper {
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100vw;
+		height: 100vh;
+		display: flex;
+		flex-flow: column;
+		overflow: hidden;
+	}
+
+	#app-nav {
+		flex: 0 0 70px;
+		display: flex;
+		align-items: center;
+	}
+
+	#app-body {
+		flex-grow: 1;
+	}
+
+	#logo {
+		font-size: 23px;
+		padding-left: 20px;
+	}
+</style>
