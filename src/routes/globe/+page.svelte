93db882efe3ea34002d463af6d07ac8d14364fc9
Tedd Arcuri <tedd.arcuri@gmail.com>
2022-11-03 00:04:29 -0600

    Adds lil' rotateGlobe util + scaffold ActiveInfoDisplay

--- src/routes/globe/+page.svelte
+++ src/routes/globe/+page.svelte
@@ -1,23 +1,25 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
+	import ActiveInfoDisplay from './ActiveInfoDisplay.svelte';
 	import { PUBLIC_MAPBOX_ACCESS_TOKEN } from '$env/static/public';
+	import rotateGlobe from './util/rotateGlobe';
 
 	onMount(async () => {
+		// Import + Initalization
 		const module = await import('mapbox-gl/dist/mapbox-gl.js');
 		const mapboxgl = module.default;
-
 		mapboxgl.accessToken = PUBLIC_MAPBOX_ACCESS_TOKEN;
-
 		const map = new mapboxgl.Map({
 			container: 'mapbox-mount',
 			style: 'mapbox://styles/mapbox/satellite-v9',
 			projection: 'globe',
-			zoom: 3.333,
+			zoom: 5,
 			bearing: 0,
 			center: [-99.94373365867199, 43.495094628394924],
 			pitch: 60
 		});
 
+		// Mapbox Loaded
 		map.on('load', () => {
 			// create DEM source
 			// https://en.wikipedia.org/wiki/Digital_elevation_model
@@ -27,21 +29,26 @@
 				tileSize: 512,
 				maxzoom: 14
 			});
+
 			// DEM source -> terrain layer
 			map.setTerrain({ source: 'mapbox-dem', exaggeration: 1.666 });
+
 			// A little fog to set the mood
 			map.setFog({
-				color: 'rgba(186, 210, 235, 0.15)', // Lower atmosphere
-				'high-color': 'rgba(36, 92, 223, 0.15)', // Upper atmosphere
+				color: 'rgba(186, 210, 235, 1)', // Lower atmosphere
+				'high-color': 'rgba(36, 92, 223, 1)', // Upper atmosphere
 				'horizon-blend': 0.0333, // Atmosphere thickness (default 0.2 at low zooms)
 				'space-color': 'rgb(0, 0, 0)', // Background color
 				'star-intensity': 0 // Background star brightness (default 0.35 at low zoooms )
 			});
+
+			rotateGlobe(map);
 		});
 	});
 </script>
 
 <div id="mapbox-mount" />
+<ActiveInfoDisplay />
 
 <style>
 	#mapbox-mount {
---
+++ src/routes/globe/ActiveInfoDisplay.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	const title = 'Document your World.';
+</script>
+
+<div
+	class="
+		absolute top-0 left-0 z-50  w-[220px] 
+		bg-zinc-900 px-3 py-2 text-center 
+		text-sm tracking-widest"
+>
+	{title}
+</div>
