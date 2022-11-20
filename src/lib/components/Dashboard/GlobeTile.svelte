bdf81320fb62be184a120e0a56b9e2db1f78c0bd
Tedd Arcuri <tedd.arcuri@gmail.com>
2022-11-19 20:56:34 -0700

    Add vanilla tilt to globe tile

--- src/lib/components/Dashboard/GlobeTile.svelte
+++ src/lib/components/Dashboard/GlobeTile.svelte
@@ -1,4 +1,16 @@
-<a href="/globe">
+<script>
+	import { onMount } from 'svelte';
+	import VanillaTilt from 'vanilla-tilt';
+
+	onMount(() => {
+		VanillaTilt.init(document.querySelector('.tilt-tile'), {
+			max: 3,
+			speed: 600
+		});
+	});
+</script>
+
+<a class="tilt-tile" href="/globe">
 	<div class="gradient" />
 	<img
 		src="https://uxjcnbzonuzmknqsevlh.supabase.co/storage/v1/object/sign/wallpapers/globe%201-min.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJ3YWxscGFwZXJzL2dsb2JlIDEtbWluLnBuZyIsImlhdCI6MTY2ODgzMzg4NCwiZXhwIjoxOTg0MTkzODg0fQ.rKwkKk8dh7h43nRR5eXzjBcgTMwx8YVJ2vdQuXlHzpw"
@@ -19,6 +31,7 @@
 
 	a span {
 		z-index: 5;
+		transform: translateZ(20px);
 	}
 
 	a {
@@ -32,11 +45,17 @@
 		border-stone-800 border-2
 		hover:border-gray-600;
 		transition: border 0.4s ease;
+		transform-style: preserve-3d;
+		transform: perspective(1000px);
+	}
+
+	a:hover {
+		box-shadow: 0 0 20px 10px rgba(154, 209, 227, 0.06);
 	}
 
 	a:active {
-		transition: border 0.2s ease;
-		@apply border-gray-700 border-[9px];
+		transition: all 0.2s ease;
+		@apply border-gray-500 border-[4px];
 	}
 
 	a:hover img {
@@ -45,7 +64,7 @@
 	}
 
 	a img {
-		transform: scale(1);
+		transform: scale(1) translateZ(20px);
 		transition: all ease 0.6s;
 		@apply absolute top-0 left-0 opacity-50 h-full w-full object-cover;
 	}
