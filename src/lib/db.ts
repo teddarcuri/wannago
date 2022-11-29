abfd6bd01b562d2624492828cbea506c649d09a1
Tedd Arcuri <tedd.arcuri@gmail.com>
2022-11-28 21:17:35 -0700

    Update db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,4 +1,7 @@
 import { createClient } from '@supabase/auth-helpers-sveltekit';
 import { env } from '$env/dynamic/public';
-
-export const supabaseClient = createClient(env.PUBLIC_SUPABASE_URL, env.PUBLIC_SUPABASE_ANON_KEY);
+// TODO: Create TS types -> https://www.youtube.com/watch?v=7CqlTU9aOR4
+export const supabaseClient = createClient(
+	env.PUBLIC_SUPABASE_URL,
+	env.PUBLIC_SUPABASE_ANON_KEY,
+);
