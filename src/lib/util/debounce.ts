09716f9ab9566aa50c5f38debe840d0bcc197df2
Tedd Arcuri <tedd.arcuri@gmail.com>
2023-02-08 20:57:10 -0700

    Create debounce.ts

---
+++ src/lib/util/debounce.ts
@@ -0,0 +1,13 @@
+const debounce = (fn, delay) => {
+	let timeoutId;
+	return (...args) => {
+		if (timeoutId) {
+			clearTimeout(timeoutId);
+		}
+		timeoutId = setTimeout(() => {
+			fn.apply(null, args);
+		}, delay);
+	};
+};
+
+export default debounce;
