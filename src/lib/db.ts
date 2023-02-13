8975f3fbd61fafaceae44a8c02d91c945df51f37
Tedd Arcuri <tedd.arcuri@gmail.com>
2023-02-13 10:15:22 -0700

    Use import.meta.env VITE env vars

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,7 +1,8 @@
 import { createClient } from '@supabase/auth-helpers-sveltekit';
 import { env } from '$env/dynamic/public';
+
 // TODO: Create TS types -> https://www.youtube.com/watch?v=7CqlTU9aOR4
 export const supabaseClient = createClient(
-	env.PUBLIC_SUPABASE_URL,
-	env.PUBLIC_SUPABASE_ANON_KEY,
+	import.meta.env.VITE_SUPABASE_URL,
+	import.meta.env.VITE_SUPABASE_ANON_KEY,
 );
